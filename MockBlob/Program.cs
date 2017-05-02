39fa351ac8bbf1c616a04a42c18977a1e2926791
Divyesh B Chudasama <divyesh@elastacloud.com>
2017-05-02 17:40:55 +0100

    First commit

--- MockBlob/Program.cs
+++ MockBlob/Program.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//Adding directives
+using Microsoft.Azure; //Namespace for CloudConfigurationManager
+using Microsoft.WindowsAzure.Storage; //Namespace for CloudStorageAccount
+using Microsoft.WindowsAzure.Storage.Blob; //Namespace for Blob storage types	
+
 
 namespace MockBlob
 {
@@ -10,6 +15,19 @@ namespace MockBlob
 	{
 		static void Main(string[] args)
 		{
+			//Parse the connection string and return a reference to the storage account
+			CloudStorageAccount storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageConnectionString"));
+			//Creating a CloudBlobClient to retrieve the container and blob storage
+			CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+			//Retrieve reference to the created container 'input'
+			CloudBlobContainer container = blobClient.GetContainerReference("input");
+			//Retrieve reference to the file arilines[2].json inside the container
+			CloudBlockBlob blockBob = container.GetBlockBlobReference("airlines[2].json");
+			//Save blob contents to a file (Download)
+			using (var fileStream = System.IO.File.OpenWrite(@"./blobDownload.json"))
+			{
+				blockBob.DownloadToStream(fileStream);
+			}
 		}
 	}
 }
