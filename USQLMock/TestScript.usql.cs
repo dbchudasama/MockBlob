cf5b64bf6206923849993aea71377996d05572fb
Divyesh B Chudasama <divyesh@elastacloud.com>
2017-05-12 16:58:06 +0100

    Shit works

--- USQLMock/TestScript.usql.cs
+++ USQLMock/TestScript.usql.cs
@@ -8,5 +8,21 @@ using System.Text;
 
 namespace USQLMock
 {
-    
+	public class Udfs
+	{
+		//<summary>
+		//calculatePercentage(SqlMap flattened 'Key' as an int. Consider using 'int.Parse(<key>), SqlMap flattened 'Key' as an int. Consider using 'int.Parse(<key>)) 
+		static public int calculatePercentage(int value1, int value2)
+		{
+			//Setting value1 to be the MAX value of the key from the Sql Map for that row
+			value1 = int.MaxValue;
+
+			//Same here
+			value2 = int.MaxValue;
+
+			//Calcualting the percentage. Divide the first value by the second and multipltying by 100
+			return ((value1 * 100) / value2);
+
+		}
+	}
 }
